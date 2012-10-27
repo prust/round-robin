b4babe6307ed9f3dae3ef36e28e0b0efaef63055
Peter Rust <peter@cornerstonenw.com>
2012-10-27 06:46:07 -0700

    Added test to verify that with 6 rounds and 7 teams, ever team plays every other team at least once

--- specs/generator.spec.js
+++ specs/generator.spec.js
@@ -4,6 +4,7 @@ var Team = require('../team.js');
 var _ = require('underscore.js');
 
 describe('round-robin generator', function() {
+  var teams, cached_combos = {};
   it('should return a nested array with all teams', function() {
     var set = genSets(1, 7)[0];
     expect(set.length).toEqual(3);
@@ -16,10 +17,24 @@ describe('round-robin generator', function() {
     expect(genSets(3, 7)).not.toEqual(genSets(3, 7));
     expect(genSets(6, 7)).not.toEqual(genSets(6, 7));
   });
+  it('should have every team play each-other at least once with 6 rounds and 7 teams', function() {
+    genSets(6, 7);
+    expect(_(getCompetitionMatrix()).flatten()).not.toContain(0);
+  })
 
+  function getCompetitionMatrix() {
+    var competition_matrix = _(teams).pluck('timesPlayedTeam');
+    nullOutTimesTeamPlayedItself(competition_matrix);
+    return competition_matrix;
+  }
+  function nullOutTimesTeamPlayedItself(comp_matrix) {
+    comp_matrix.forEach(function(timesPlayedTeam, team_ix) {
+      timesPlayedTeam[team_ix] = null;
+    });
+  }
   function genSets(num_sets, num_teams) {
-    var teams = createTeams(num_teams);
-    var combos = ComboCollection.createCombos(teams);
+    teams = createTeams(num_teams);
+    var combos = createCombos(teams);
     return _.range(num_sets).map(function() {
       return genSet(teams, combos);
     });
@@ -37,5 +52,11 @@ describe('round-robin generator', function() {
       return 'Team ' + team_num;
     });
     return Team.createFromNames(team_names);
+  }
+  function createCombos(teams) {
+    var num_teams = teams.length; // sufficient until we start dealing w/ inactive teams
+    if (!cached_combos[num_teams])
+      cached_combos[num_teams] = ComboCollection.createCombos(teams);
+    return cached_combos[num_teams];
   }
 });
