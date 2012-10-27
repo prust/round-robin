eef7dd9ac915048a72dca1af86635b2f3c9481a5
Peter Rust <peter@cornerstonenw.com>
2012-10-27 06:27:28 -0700

    Refactored tests & added test for 7 teams and 6 rounds

--- specs/generator.spec.js
+++ specs/generator.spec.js
@@ -4,38 +4,27 @@ var Team = require('../team.js');
 var _ = require('underscore.js');
 
 describe('round-robin generator', function() {
-  var teams, combos, num_teams = 7;
-  beforeEach(function() {
-    resetTeams();
-    combos = ComboCollection.createCombos(teams);
-  });
-  
   it('should return a nested array with all teams', function() {
-    var set = genSet();
+    var set = genSets(1, 7)[0];
     expect(set.length).toEqual(3);
     expect(_.flatten(set).length).toEqual(7);
   });
   it('should not repeat identical sets on the first run', function() {
-    var first_set = genSet();
-    resetTeams();
-    var second_set = genSet();
-    expect(first_set).not.toEqual(second_set);
+    expect(genSets(1, 7)).not.toEqual(genSets(1, 7));
   });
   it('should not create identical sets after multiple runs', function() {
-    genSet();
-    genSet();
-    var third_run_1 = genSet();
-
-    resetTeams();
-    genSet();
-    genSet();
-    var third_run_2 = genSet();
-    expect(third_run_1).not.toEqual(third_run_2);
+    expect(genSets(3, 7)).not.toEqual(genSets(3, 7));
+    expect(genSets(6, 7)).not.toEqual(genSets(6, 7));
   });
-  function resetTeams() {
-    teams = createTeams(num_teams);
+
+  function genSets(num_sets, num_teams) {
+    var teams = createTeams(num_teams);
+    var combos = ComboCollection.createCombos(teams);
+    return _.range(num_sets).map(function() {
+      return genSet(teams, combos);
+    });
   }
-  function genSet() {
+  function genSet(teams, combos) {
     var set;
     generator.genRound({'teams': teams, 'combos': combos}, function(result) {
       set = result.best_set;
